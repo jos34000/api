ac28e4d3b2afd386884bdc1f0a9b9fb1b98ba80b
jos34000 <jocelynsainson@icloud.com>
2024-02-01 18:11:09 +0100

    Suppression des fichiers inutiles et des configurations obsolètes

--- pages/api/findSpecialites.js
+++
@@ -1,8 +0,0 @@
-import { PrismaClient } from "@prisma/client"
-const prisma = new PrismaClient()
-
-export default async function getSpecialites(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*")
-  const specialites = await prisma.specialite.findMany({})
-  res.json(specialites)
-}
--- pages/api/hello.js
+++
@@ -1,8 +0,0 @@
-// export default (req, res) => {
-//   res.status(200).json({ message: "Hello" })
-// }
-
-export default (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*")
-  return res.json({ message: "Hello" })
-}
