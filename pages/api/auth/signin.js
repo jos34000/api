8d5967b91c1b564a004c13209edfed3de6e17837
jos34000 <jocelynsainson@icloud.com>
2024-02-27 18:24:06 +0100

    Mise à jour des fonctionnalités de recherche***

---
+++ pages/api/auth/signin.js
@@ -0,0 +1,28 @@
+import { PrismaClient } from "@prisma/client"
+const prisma = new PrismaClient()
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    const { email, password } = req.body
+
+    try {
+      const user = await prisma.user.findUnique({
+        where: {
+          email,
+        },
+      })
+
+      if (!user || user.password !== password) {
+        return res
+          .status(401)
+          .json({ success: false, error: "Invalid credentials" })
+      }
+
+      return res.status(200).json({ success: true, user })
+    } catch (error) {
+      return res.status(500).json({ success: false, error: error.message })
+    }
+  } else {
+    res.status(405).json({ success: false, error: "Method not allowed" })
+  }
+}
---
+++ pages/api/auth/signup.js
@@ -0,0 +1,25 @@
+import { PrismaClient } from "@prisma/client"
+const prisma = new PrismaClient()
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    const { email, password, name, surname } = req.body
+
+    try {
+      const patient = await prisma.patient.create({
+        data: {
+          email,
+          password,
+          name,
+          surname,
+        },
+      })
+
+      return res.status(200).json({ success: true, user })
+    } catch (error) {
+      return res.status(500).json({ success: false, error: error.message })
+    }
+  } else {
+    res.status(405).json({ success: false, error: "Method not allowed" })
+  }
+}
