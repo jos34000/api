52991b5fdf9c8d4106a7987ba2e407e2352eec29
jos34000 <jocelynsainson@icloud.com>
2024-03-28 12:02:36 +0100

    Mise à jour de next.config.js avec l'importation de dotenv et la configuration de l'environnement

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
+import { config } from "dotenv"
+import { resolve } from "path"
+
+let envFile
+switch (process.env.NODE_ENV) {
+  case "dev":
+    envFile = ".env.dev"
+    break
+  case "home":
+    envFile = ".env.home"
+    break
+  default:
+    envFile = ".env.prod"
+}
+
+config({ path: resolve("/api", envFile) })
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+export default nextConfig
